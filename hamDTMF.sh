ec1c149382cd773187b2f5cfe0d7883310e8186e
Shayne - VK3ACN <shayne.nash@gmail.com>
2023-10-03 01:17:53 +1100

    Update hamDTMF.sh

--- hamDTMF.sh
+++ hamDTMF.sh
@@ -4,18 +4,18 @@ callsign="Victor Charlie 3 Alpha Charlie November - VK3ACN"
 
 msg="Welcome to the VK3 ACN. Victor Kilo 3 Alpha Charlie November Automated System Menu. Press 1 or Pound to hear this message again. Press 2 for the current local date and time. Press 4 for the current CPU Usage. Press 5 for the current computer uptime. Press 7 for contact information. Press 8 for a tongue twister. Enter one time code and hash to deactivate dead man switch"
 
-contactinfo="VK3 ACN. Victor Kilo 3 Alpha Charlie November... E Mail - S H A Y N E DOT N A S H AT G MAIL DOT COM. PRESS 7 TO REPEAT"
+contactinfo="VK3 ACN. Victor Kilo 3 Alpha Charlie November... E Mail - S H A Y N E DOT N A S H AT G MAIL DOT COM. The code for this system can be found on git hub as follows. H T T P S COLON SLASH SLASH G I T H U B DOT COM SLASH P E N R E B L SLASH H A M D T M F.  PRESS 7 TO REPEAT"
 
 #Define Tongue Twisters
 ttwister[0]="I have got a date at a quarter to eight; I'll see you at the gate, so don't be late."
 ttwister[1]="I scream, you scream, we all scream for ice cream."
 ttwister[2]="Xander the xylophonist played his xylophone for the fox."
 ttwister[3]="I wish to wish the wish you wish to wish, but if you wish the wish the witch wishes, I won’t wish the wish you wish to wish."
-ttwister[3]="If two witches were watching two watches, which witch would watch which watch?"
-ttwister[3]="Karl’s colorful kite caught a colossal king-sized koala"
-ttwister[3]="A box of biscuits, a box of mixed biscuits, and a biscuit mixer"
-ttwister[3]="The seething sea ceaseth and thus the seething sea sufficeth us."
-ttwister[3]="Theophilus Thistler the thistle sifter, In sifting a sieveful of unsifted thistles thrust three thousand Thistles through the thick of his thumb."
+ttwister[4]="If two witches were watching two watches, which witch would watch which watch?"
+ttwister[5]="Karl’s colorful kite caught a colossal king-sized koala"
+ttwister[6]="A box of biscuits, a box of mixed biscuits, and a biscuit mixer"
+ttwister[7]="The seething sea ceaseth and thus the seething sea sufficeth us."
+ttwister[8]="Theophilus Thistler the thistle sifter, In sifting a sieveful of unsifted thistles thrust three thousand Thistles through the thick of his thumb."
 
 
 
@@ -23,7 +23,7 @@ nc -l -u 7355 | sox -t raw -esigned-integer -b16 -r 48000 - -esigned-integer -b1
 
 while read line;
 	do
-		echo $line
+		echo $line /n
 	
 		case $line in
 			"DTMF: 1")
